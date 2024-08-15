ae1903df83669e5245a2d9e3a7b62b357bb9d3cd
igorbdaluz <igorbdaluz@hotmail.com>
2024-08-15 18:37:37 -0300

    Criando a classe abstrata de login para manter a sessão no Windows Forms.

---
+++ VisualBasicLib/Abstracts/LoginAbstract.vb
@@ -0,0 +1,16 @@
+﻿Imports EntityFrameworkLib.Models
+
+Namespace Abstracts
+  Public MustInherit Class LoginAbstract
+    Private _token As String
+    Public Property Token() As String
+      Get
+        Return _token
+      End Get
+      Set(value As String)
+        _token = value
+      End Set
+    End Property
+    Public MustOverride Sub Login(usuario As Usuario)
+  End Class
+End Namespace
