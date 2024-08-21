337ba7a2757aec5614da2e275a8616aa06322709
igorbdaluz <igorbdaluz@hotmail.com>
2024-08-20 22:01:43 -0300

    Colocando o Ninject dentro do projeto central em .NET Standard. Agora devo tornar possível utilizar em cada projeto a injeção de dependência separada para cada tipo: .NET Framework 4.7.2 e .NET 8. #5

---
+++ VisualBasicLib/Classes/NinjectDI.vb
@@ -0,0 +1,27 @@
+﻿Imports EntityFrameworkLib.Context
+Imports EntityFrameworkLib.Models
+Imports Ninject
+Imports Ninject.Modules
+Imports VisualBasicLib.Abstracts
+Imports VisualBasicLib.Repositories
+Imports VisualBasicLib.ViewModels
+
+Namespace Classes
+  Public Class NinjectDI
+    Inherits NinjectModule
+
+    Private Property _kernel As IKernel
+
+    Public Sub New()
+      _kernel = New StandardKernel()
+    End Sub
+
+    Public Overrides Sub Load()
+      Bind(Of LibDbContext).ToSelf()
+      Bind(Of RepositoryAbstract(Of Pessoa)).To(Of PessoaRepository)()
+      Bind(Of RepositoryAbstract(Of Endereco)).To(Of EnderecoRepository)()
+      Bind(Of TypeTViewModel(Of Pessoa)).To(Of PessoaViewModel)()
+      Bind(Of TypeTViewModel(Of Endereco)).To(Of EnderecoViewModel)()
+    End Sub
+  End Class
+End Namespace
