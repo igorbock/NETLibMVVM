3b8b058a8e641949a223c8656cf913294727820e
igorbdaluz <igorbdaluz@hotmail.com>
2024-08-21 22:03:17 -0300

    Tentando colocar o container de dependências do .NET Standard com Ninject para o .NET Core com MAUI.

---
+++ VisualBasicLib/Classes/NinjectConfig.vb
@@ -0,0 +1,11 @@
+﻿Imports Ninject
+Imports VisualBasicLib.Classes
+
+Public Module NinjectConfig
+  Public Function CreateKernel() As IKernel
+    Dim kernel As IKernel = New StandardKernel()
+    Dim dependencies As New NinjectDI()
+    dependencies.OnLoad(kernel)
+    Return kernel
+  End Function
+End Module
--- VisualBasicLib/Classes/NinjectDI.vb
+++ VisualBasicLib/Classes/NinjectDI.vb
@@ -1,6 +1,5 @@
 ﻿Imports EntityFrameworkLib.Context
 Imports EntityFrameworkLib.Models
-Imports Ninject
 Imports Ninject.Modules
 Imports VisualBasicLib.Abstracts
 Imports VisualBasicLib.Repositories
@@ -9,13 +8,6 @@ Imports VisualBasicLib.ViewModels
 Namespace Classes
   Public Class NinjectDI
     Inherits NinjectModule
-
-    Private Property _kernel As IKernel
-
-    Public Sub New()
-      _kernel = New StandardKernel()
-    End Sub
-
     Public Overrides Sub Load()
       Bind(Of LibDbContext).ToSelf()
       Bind(Of RepositoryAbstract(Of Pessoa)).To(Of PessoaRepository)()
