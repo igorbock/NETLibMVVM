1e571836a8a0b4dcdbbac13ea50c77f87034034c
igorbdaluz <igorbdaluz@hotmail.com>
2024-09-01 12:02:43 -0300

    Criando o repository de exercicio e treino. #6

---
+++ VisualBasicLib/Repositories/ExercicioRepository.vb
@@ -0,0 +1,37 @@
+﻿Imports EntityFrameworkLib.Context
+Imports EntityFrameworkLib.Models.Academia
+Imports VisualBasicLib.Abstracts
+
+Namespace Repositories
+  Public Class ExercicioRepository
+    Inherits RepositoryAbstract(Of Exercicio)
+
+    Public Sub New(dbContext As LibDbContext)
+      MyBase.New(dbContext)
+    End Sub
+
+    Public Overrides Sub Insert(entity As Exercicio)
+      _dbContext.Exercicios.Add(entity)
+    End Sub
+
+    Public Overrides Sub Update(entity As Exercicio)
+      _dbContext.Entry(entity).State = Microsoft.EntityFrameworkCore.EntityState.Modified
+    End Sub
+
+    Public Overrides Sub Delete(id As Integer)
+      _dbContext.Exercicios.Remove(GetById(id))
+    End Sub
+
+    Public Overrides Sub Cancel(entity As Exercicio)
+      _dbContext.Entry(entity).Reload()
+    End Sub
+
+    Public Overrides Function GetAll() As ObjectModel.ObservableCollection(Of Exercicio)
+      Return New ObjectModel.ObservableCollection(Of Exercicio)(_dbContext.Exercicios.ToList())
+    End Function
+
+    Public Overrides Function GetById(id As Integer) As Exercicio
+      Return _dbContext.Exercicios.Find(id)
+    End Function
+  End Class
+End Namespace
---
+++ VisualBasicLib/Repositories/TreinoRepository.vb
@@ -0,0 +1,37 @@
+﻿Imports EntityFrameworkLib.Context
+Imports EntityFrameworkLib.Models.Academia
+Imports VisualBasicLib.Abstracts
+
+Namespace Repositories
+  Public Class TreinoRepository
+    Inherits RepositoryAbstract(Of Treino)
+
+    Public Sub New(dbContext As LibDbContext)
+      MyBase.New(dbContext)
+    End Sub
+
+    Public Overrides Sub Insert(entity As Treino)
+      _dbContext.Treinos.AddRange(entity)
+    End Sub
+
+    Public Overrides Sub Update(entity As Treino)
+      _dbContext.Entry(entity).State = Microsoft.EntityFrameworkCore.EntityState.Modified
+    End Sub
+
+    Public Overrides Sub Delete(id As Integer)
+      _dbContext.Treinos.Remove(GetById(id))
+    End Sub
+
+    Public Overrides Sub Cancel(entity As Treino)
+      _dbContext.Entry(entity).Reload()
+    End Sub
+
+    Public Overrides Function GetAll() As ObjectModel.ObservableCollection(Of Treino)
+      Return New ObjectModel.ObservableCollection(Of Treino)(_dbContext.Treinos.ToList())
+    End Function
+
+    Public Overrides Function GetById(id As Integer) As Treino
+      Return _dbContext.Treinos.Find(id)
+    End Function
+  End Class
+End Namespace
